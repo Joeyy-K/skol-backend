48e8f0ac537d61f3ba8cb99895bf7553466f2e2c
joeyy-k <joeykkdr@gmail.com>
2025-09-05 00:45:55 +0300

    updated build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # Exit on error
 set -o errexit
 
+echo "---> Running build script..."
+
 # Install dependencies
 pip install -r requirements.txt
 
@@ -10,3 +12,15 @@ python manage.py collectstatic --no-input
 
 # Apply any database migrations
 python manage.py migrate
+
+# --- NEW LOGIC: Conditionally populate the database ---
+# We will check for an environment variable called POPULATE_DB
+# If it's set to "true", we run the scripts.
+if [[ $POPULATE_DB == "true" ]]; then
+  echo "---> POPULATE_DB is true, running population scripts..."
+  python manage.py populate_db --force
+  python manage.py populate_fees --force
+  echo "---> Database population complete."
+fi
+
+echo "---> Build script finished."
