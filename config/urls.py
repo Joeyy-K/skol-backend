413650face78a7ca273704bab7fedef071d4010b
joeyy-k <joeykkdr@gmail.com>
2025-10-29 19:35:41 +0300

    made changes to config.url

--- config/urls.py
+++ config/urls.py
@@ -42,6 +42,10 @@ urlpatterns = [
     path('api/parents/', include('parents.urls')),
     path('api/reports/', include('reports.urls')),
     path('api/fees/', include('fees.urls')), 
+    path('api/expenses/', include('expenses.urls')),
+    path('api/budgets/', include('budgets.urls')), 
+    path('api/calendar-events/', include('calendar_events.urls')),
+    path('api/notifications/', include('notifications.urls')),
     path('api/', include('subjects.urls')),
     path('api/', include('exams.urls')), 
 ]
